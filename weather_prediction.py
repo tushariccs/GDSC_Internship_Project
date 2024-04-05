5b1ea6af14895fdcbaadb74f43fdc7e536d5c1fe
Tushar Bhansali <103886040+tushariccs@users.noreply.github.com>
2024-04-05 19:51:41 +0530

    Done

--- weather_prediction.py
+++ weather_prediction.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
+# import plotly.express as px
 import matplotlib
 matplotlib.use('TkAgg')
 #   
